849692ca2271cb59cf040f6977eca29a2499a86d
Nishantjaryal <nishantjaryal24@gmail.com>
2025-07-12 09:24:37 +0530

    Update github_chart.tsx

--- components/ui/github_chart.tsx
+++ components/ui/github_chart.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const GithubChart = () => {
   return (
-    <div className="w-full hidden lg:flex items-center justify-center mb-10">
+    <div className="w-full hidden lg:flex items-center justify-center transition-all mb-10">
       <img
         src="https://ghchart.rshah.org/2095ff/Nishantjaryal"
         alt=""
-        className="w-[65%] min-w-[320px] h-auto invert hue-rotate-90 saturate-75 contrast-100"
+        className="w-[65%] transition-all min-w-[320px] h-auto invert hue-rotate-90 saturate-75 contrast-100"
         fetchPriority="high"
       />
     </div>

d29444083673108da53b7bfdf8f46a2f28213e12
NISHANT SINGH <nishantjaryal24@gmail.com>
2026-01-31 18:02:05 +0530

    patch fixes

--- next.config.ts
+++ next.config.ts
@@ -37,7 +37,6 @@ enabled: true,
 // tunnelRoute: "/monitoring",
 
 // Hides source maps from generated client bundles
-hideSourceMaps: true,
 
 // Automatically tree-shake Sentry logger statements to reduce bundle size
 disableLogger: true,
